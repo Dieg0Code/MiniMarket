1a8fdf909c6daaa8ea972e7cc7666bc7fc282247
fab1an111111 <fabian.limache1@gmail.com>
2021-12-12 00:00:03 -0300

    SQL/spCliente comas cambiadas

--- SQLMiniMarket/spDetalleVenta.sql
+++ SQLMiniMarket/spDetalleVenta.sql
@@ -10,9 +10,10 @@ create proc spguardar_DetalleVenta
 @ID_DET_VENTA int output,
 @ID_VENTA int,
 @ID_PRODUCTO int, 
-@DET_VEN_CANTIDAD int,
+@DET_VEN_CANTIDAD int
 as
-insert into DETALLE_VENTA (ID_VENTA,
+insert into DETALLE_VENTA (
+ID_VENTA,
 ID_PRODUCTO,
 DET_VEN_CANTIDAD,
 ) values (@ID_VENTA, @ID_PRODUCTO, @DET_VEN_CANTIDAD)
@@ -23,12 +24,12 @@ create proc spguardar_DetalleVenta
 @ID_DET_VENTA int,
 @ID_VENTA int,
 @ID_PRODUCTO int,
-@DET_VEN_CANTIDAD int,
+@DET_VEN_CANTIDAD int
 as
 update DETALLE_VENTA set 
 ID_VENTA = @ID_VENTA,
 ID_PRODUCTO = @ID_PRODUCTO,
-DET_VEN_CANTIDAD = @DET_VEN_CANTIDAD 
+DET_VEN_CANTIDAD = @DET_VEN_CANTIDAD
 where ID_DET_VENTA = @ID_DET_VENTA 
 go
 
